eca2b9cef7c6d52deb1fae48b172761da84d1964
KevinJairChuquitarco <kevinchuquitarco11@gmail.com>
2024-12-12 10:16:22 -0500

    Final IMc

--- App.js
+++ App.js
@@ -1,13 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, Image } from 'react-native';
+import { NativeBaseProvider, Box, Input } from 'native-base';
 
 export default function App() {
+
+  const [peso, setPeso] = useState();
+  const [altura, setAltura] = useState();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>Caculadora IMC</Text>
+        <Image
+          style={styles.imagen}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        ></Image>
+        <Box alignItems="center">
+          <Input ></Input>
+        </Box>
+        <TextInput
+          placeholder='Ingrese su peso (Kg)'
+          keyboardType='numeric'
+          onChangeText={(value) => setPeso(value)}
+        ></TextInput>
+        <TextInput
+          placeholder='Ingrese su altura (m)'
+          keyboardType='numeric'
+          onChangeText={(value) => setAltura(value)}
+        ></TextInput>
+        <Button
+          title='Calcular'
+          onPress={() => Alert.alert("Alerta", calcularNivelPeso(peso, altura))}
+        ></Button>
+      </View>
+    </NativeBaseProvider>
   );
+}
+
+const calcularIMC = (peso, altura) => {
+  let IMC = peso / (altura * altura);
+  console.log(IMC);
+  return IMC.toFixed(2);
+}
+
+const calcularNivelPeso = (peso, altura) => {
+  let IMC = calcularIMC(peso, altura);
+  let respuesta = "";
+  if (IMC < 18.5) {
+    respuesta = "Su IMC es " + IMC + " Peso inferior al normal";
+  }
+  if (IMC >= 18.5 && IMC <= 24.9) {
+    respuesta = "Su IMC es " + IMC + " Normal";
+  }
+  if (IMC >= 25 && IMC <= 29.9) {
+    respuesta = "Su IMC es " + IMC + " Peso superior al normal";
+  }
+  if (IMC >= 30) {
+    respuesta = "Su IMC es " + IMC + " Obesidad";
+  }
+  return respuesta;
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +70,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imagen: {
+    width: 100,
+    height: 100
+  }
 });
